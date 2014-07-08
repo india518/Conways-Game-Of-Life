91c8b46cd1551764db21b4802dcaf4764a2abe73
India Meisner <india.rae@gmail.com>
2014-07-07 20:10:39 -0700

    can create a Grid object containing tiles as a 2-dimensional array

--- grid.php
+++ grid.php
@@ -2,5 +2,31 @@
 
 Class Grid
 {
-	
+	var $grid;
+	var $size;
+	var $generation;
+
+	function __construct($size)
+	{
+		$this->size = $size;
+		$this->create_empty_grid($size);
+		$this->generation = 0;
+	}
+
+	//create a 2-dim array of size $size
+	//for now, grid will initially be full of non-live Tiles
+	function create_empty_grid($size)
+	{
+		$grid = [];
+		for($x=0; $x<$size; $x++)
+		{
+			$grid[$x] = [];
+			for($y=0; $y<$size; $y++)
+			{
+				$tile = new Tile($x, $y);
+				array_push($grid[$x], $tile);
+			}
+		}
+		$this->grid = $grid;
+	}
 }
--- index.php
+++ index.php
@@ -10,30 +10,19 @@
 	<link rel="stylesheet" type="text/css" href="life.css" />
 </head>
 <body>
+	<?php
+		//start small
+		$grid = new Grid(2);
+
+		echo "<pre>";
+		var_dump($grid->grid);
+
+	?>
 	<form action="process.php" method="POST">
 		<input type="submit" value="Start over" />
 	</form>
 	<h1>Generation PUT_GENERATION_NUMBER_HERE</h1>
 	<h4>Refresh page to advance to the next generation</h4>
-	<h5>A test of tiles</h5>
-	<?php
-		$tile11 = new Tile(1,1); // "dead" tile
-		$tile67 = new Tile(6,7, TRUE) // "live" tile
-	?>
-	<p>Info on our "dead" tile:</p>
-	<ul>
-		<li>x co-ordinate: <?= $tile11->x ?></li>
-		<li>y co-ordinate: <?= $tile11->y ?></li>
-		<li>state: <?php echo $tile11->state ? "ALIVE" : "DEAD" ?>
-	</ul>
-
-	<p>Info on our "live" tile:</p>
-	<ul>
-		<li>x co-ordinate: <?= $tile67->x ?></li>
-		<li>y co-ordinate: <?= $tile67->y ?></li>
-		<li>state: <?php echo $tile67->state ? "ALIVE" : "DEAD" ?>
-	</ul>
-
 	<table>
 		<tbody>
 		</tbody>
