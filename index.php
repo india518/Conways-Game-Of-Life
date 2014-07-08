12f9d9d2b41b7a922e4cc664c52cd74dccc7c23a
India Meisner <india.rae@gmail.com>
2014-07-07 19:48:30 -0700

    Initial commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Game of Life</title>
+	<link rel="stylesheet" type="text/css" href="life.css" />
+</head>
+<body>
+	<form action="process.php" method="POST">
+		<input type="submit" value="Start over" />
+	</form>
+	<h1>Generation PUT_GENERATION_NUMBER_HERE</h1>
+	<h4>Refresh page to advance to the next generation</h4>
+	<table>
+		<tbody>
+		</tbody>
+	</table>
+</body>
+</html>
