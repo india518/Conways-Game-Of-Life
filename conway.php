f51be38f34c1932bfccede1f906b2725abcd89e0
India Meisner <india.rae@gmail.com>
2014-07-14 15:07:06 -0700

    Add Bootstrap (prettier) and made the restart button a link instead

--- conway.php
+++ conway.php
@@ -8,47 +8,47 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Game of Life</title>
+	<link rel="stylesheet" type="text/css" href="/css/bootstrap.min.css" />
 	<link rel="stylesheet" type="text/css" href="/css/life.css" />
+	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
 </head>
 <body>
-	<?php
-		if(isset($_SESSION['grid']))
-		{
-			$grid = unserialize($_SESSION['grid']);
-		}
-		else
-		{
-			//new 'game': create new grid, seed it with a shape
-			//$starting_shape = array([0,1], [1,0], [2,1], [2,2]);
-			$glider = array([1,3],[2,3],[3,3],[3,2],[2,1]);
-			$grid = new Grid(50, $glider);
-		}
-	?>
-	<h2>Generation <?= $grid->generation ?></h2>
-	<h5>Refresh page to advance to the next generation</h5>
-	<h5 class="inline">Restart the game:</h5>
-	<form class="inline" action="process.php" method="POST">
-		<input type="submit" value="Restart!" />
-	</form>
-	<table>
-		<tbody>
-			<?= $grid->display_grid() ?>
-		</tbody>
-	</table>
-	<?php
-		$grid->prepare_next_generation();
-	?>
-	<!-- <p>advancing the next generation, and displaying it (for testing!)</p> -->
-	<?php
-		$grid->advance_generation();
-		//store the grid in session for the page reload:
-		$_SESSION['grid'] = serialize($grid);
-		//session_unset('grid');
-	?>
-	<!-- <table>
-		<tbody>
-			<?//= $grid->display_grid() ?>
-		</tbody>
-	</table> -->
+	<div class="container">
+		<?php
+			if(isset($_SESSION['grid']))
+			{
+				$grid = unserialize($_SESSION['grid']);
+			}
+			else
+			{
+				//new 'game': create new grid, seed it with a shape
+				//$starting_shape = array([0,1], [1,0], [2,1], [2,2]);
+				$glider = array([1,3],[2,3],[3,3],[3,2],[2,1]);
+				$grid = new Grid(25, $glider);
+			}
+		?>
+		<h2>Generation <?= $grid->generation ?></h2>
+		<h5>Refresh page to advance to the next generation, or <a href="process.php">Restart</a> the game</h5>
+		<table>
+			<tbody>
+				<?= $grid->display_grid() ?>
+			</tbody>
+		</table>
+		<?php
+			$grid->prepare_next_generation();
+			$grid->advance_generation();
+			//store the grid in session for the page reload:
+			$_SESSION['grid'] = serialize($grid);
+			//session_unset('grid');
+		?>
+		<!-- <table>
+			<tbody>
+				<?//= $grid->display_grid() ?>
+			</tbody>
+		</table> -->
+	</div>
 </body>
 </html>
